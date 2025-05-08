ebe6a494cbb984bc969125c001f7c33b0810255f
KBG1 <bumdoly200@naver.com>
2025-05-08 16:18:50 +0900

    design: 로그인 페이지 퍼블리싱

--- insty-frontend/src/app/login/_components/LoginHeader.tsx
+++ insty-frontend/src/app/login/_components/LoginHeader.tsx
@@ -4,7 +4,7 @@ function LoginHeader() {
 	return (
 		<div className="flex justify-between items-center px-8 py-2 shadow-line-100">
 			<Image src="/insty.png" alt="logo" width={72} height={63} />
-			<button className="h-8 bg-primary-blue-300 hover:bg-primary-blue-500 text-white border px-8 py-2 rounded-[16px] flex items-center">
+			<button className="h-8 bg-primary-blue-300 hover:bg-primary-blue-500 cursor-pointer text-white border px-8 py-2 rounded-[16px] flex items-center">
 				로그인
 			</button>
 		</div>
