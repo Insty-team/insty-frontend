d4d719e2f426d68a5613af1cc9861ece403d7c4e
dev-junehee <dev.junehee@gmail.com>
2025-05-29 21:28:36 +0900

    design: 러너 > 마이페이지 > 나의 정보 관리 퍼블리싱

---
+++ insty-frontend/src/app/learner/mypage/_components/MyPageLearnerSide.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { LEARNER_MYPAGE_MENU_LIST } from "@/app/constants/constants";
+
+function MyPageLearnerSide() {
+	const pathname = usePathname();
+
+	return (
+		<>
+			<div className="flex flex-col gap-10">
+				<span className="text-3xl font-bold">마이페이지</span>
+				<div className="flex flex-col gap-10">
+					{LEARNER_MYPAGE_MENU_LIST.map((menu) => (
+						<Link href={`${menu.path}`} key={menu.id}>
+							<span className={pathname === menu.path ? "font-semibold" : ""}>
+								{menu.title}
+							</span>
+						</Link>
+					))}
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default MyPageLearnerSide;
