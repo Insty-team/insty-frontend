35d18f21776d4c717076d71650cd72afffff2f09
dev-junehee <dev.junehee@gmail.com>
2025-06-03 20:40:13 +0900

    refactor: 크리에이터 > 마이페이지 > 라우팅 대신 메뉴 선택값에 따라 하위 컴포넌트 렌더링 방식으로 수정

--- insty-frontend/src/app/learner/mypage/page.tsx
+++ insty-frontend/src/app/learner/mypage/page.tsx
@@ -1,8 +1,6 @@
-import { redirect } from "next/navigation";
-
 // 마이페이지 진입 시 '나의 정보 관리'로 리다이렉트 처리
 function MyPage() {
-	redirect("/learner/mypage/profile");
+	return <></>;
 }
 
 export default MyPage;
