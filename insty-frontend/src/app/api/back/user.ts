59264f2c6b40b24e36709151c788128a67114a7f
KBG1 <bumdoly200@naver.com>
2025-06-07 17:16:36 +0900

    feat : 로그인 및 회원가입 api 구현

---
+++ insty-frontend/src/app/api/back/user.ts
@@ -0,0 +1,33 @@
+import { SignupForm } from "@/app/types/index.d";
+import axios from "axios";
+
+//닉네임 중복 체크
+export const getNicknameCheck = async (nickname: string) => {
+	const res = await axios.get(`${process.env.NEXT_PUBLIC_BACK_API_URL}/users/nickname/check`, {
+		params: {
+			nickname,
+		},
+	});
+
+	return res.data.data;
+};
+
+//이메일 중복 체크
+export const getEmailCheck = async (email: string) => {
+	const res = await axios.get(`${process.env.NEXT_PUBLIC_BACK_API_URL}/users/email/check`, {
+		params: {
+			email,
+		},
+	});
+
+	return res.data.data;
+};
+
+//회원 가입
+export const postSignup = async (data: SignupForm) => {
+	const res = await axios.post(`${process.env.NEXT_PUBLIC_BACK_API_URL}/users`, data);
+
+	return res.data.data;
+};
+
+
