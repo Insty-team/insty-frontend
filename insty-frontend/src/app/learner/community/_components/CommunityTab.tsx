b51670e816e6ee390451361783a647974b4fa4c6
dev-junehee <dev.junehee@gmail.com>
2025-06-07 11:54:17 +0900

    design: 러너 > 커뮤니티 퍼블리싱

---
+++ insty-frontend/src/app/learner/community/_components/CommunityTab.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+	BaseButton,
+	BaseSearchBar,
+	CommunityVideo,
+} from "@/app/_components/common";
+import CommunityQuestion from "@/app/_components/common/CommunityQuestion";
+import { ChangeEvent, useState } from "react";
+
+function CommunityTab() {
+	const [value, setValue] = useState("");
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+	return (
+		<div className="flex flex-col gap-4">
+			<div className="flex justify-between items-start">
+				<h3 className="text-2xl font-semibold">커뮤니티</h3>
+				<div className="w-[260px]">
+					<BaseButton title="질문 남기기" textSize="text-2lg" />
+				</div>
+			</div>
+			<BaseSearchBar
+				value={value}
+				placeholder="원하는 질문이나 키워드를 입력하세요!"
+				onChange={onChange}
+			/>
+			{/* 내가 구매한 영상 + 질문/답변 */}
+			<div className="flex gap-10 mt-10">
+				<div className="flex flex-col gap-4">
+					<span className="text-xl font-bold">내가 구매한 영상</span>
+					{[
+						{
+							title: "제목",
+							content: "내용",
+							isSelected: true,
+						},
+						{
+							title: "제목",
+							content: "내용",
+							isSelected: false,
+						},
+					].map((item) => (
+						<CommunityVideo
+							key={item.title}
+							title={item.title}
+							content={item.content}
+							isSelected={item.isSelected}
+						/>
+					))}
+				</div>
+				<div className="flex flex-col gap-10">
+					{[1, 2, 3].map((item) => (
+						<CommunityQuestion key={item} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CommunityTab;
---
+++ insty-frontend/src/app/learner/community/_components/FreeBoardTab.tsx
@@ -0,0 +1,5 @@
+function FreeBoardTab() {
+	return <div>자유게시판</div>;
+}
+
+export default FreeBoardTab;
