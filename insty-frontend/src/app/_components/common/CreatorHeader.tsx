1f039b5b93050fd949c68a49ecb46530210bd9e3
dev-junehee <dev.junehee@gmail.com>
2025-06-08 17:48:31 +0900

    refactor: 헤더 > 프로필 아이콘 react icon 적용

--- insty-frontend/src/app/_components/common/CreatorHeader.tsx
+++ insty-frontend/src/app/_components/common/CreatorHeader.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FaCircleUser } from "react-icons/fa6";
+import { GoBellFill } from "react-icons/go";
 
 import { CREATOR_MENU_LIST } from "@/app/constants";
 
@@ -47,23 +49,10 @@ function CreatorHeader() {
 					</div>
 				</div>
 				<div className="flex gap-8 justify-center items-center">
-					<Image
-						className="cursor-pointer"
-						src="/alram.svg"
-						alt="alram"
-						width={36}
-						height={36}
-					/>
+					<GoBellFill className="cursor-pointer size-8 text-gray-300" />
 					<Link href={mypage.path} key={mypage.id}>
-						<Image
-							className="cursor-pointer"
-							src="/profile.svg"
-							alt="profile"
-							width={36}
-							height={36}
-						/>
+						<FaCircleUser className="cursor-pointer size-7.5 text-gray-300" />
 					</Link>
-
 					<span className="--text-2lg font-medium">김가나</span>
 				</div>
 			</div>
