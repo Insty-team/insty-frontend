5c26b1d1b24161ac076dc9d6f72d562a12764554
KBG1 <bumdoly200@naver.com>
2025-06-13 17:03:10 +0900

    fix: 로그인 예외 처리 추가

--- src/app/api/backend/auth.ts
+++ src/app/api/backend/auth.ts
@@ -1,10 +1,29 @@
-import axios from "axios";
-
 import { LoginForm, LoginResponse } from "@/app/types/index.d";
+import axios from "axios";
+import axiosInstance from "../interceptor";
+import { ApiResponse } from "@/app/types/api";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BACK_BASE_URL;
 
-export const postLogin = async (body: LoginForm): Promise<LoginResponse> => {
-	const res = await axios.post(`${BASE_URL}/auth/login`, body);
+const postLogin = async (data: LoginForm) => {
+	try {
+		const res = await axios.post<ApiResponse<LoginResponse>>(
+			`${BASE_URL}/auth/login`,
+			data,
+		);
+		return res.data;
+	} catch (error) {
+		if (axios.isAxiosError(error) && error.response) {
+			return error.response.data;
+		}
+
+		throw new Error("서버와 통신 불가");
+	}
+};
+
+const postLogout = async () => {
+	const res = await axiosInstance.post(`${BASE_URL}/auth/logout`);
 	return res.data.data;
 };
+
+export { postLogin, postLogout };
