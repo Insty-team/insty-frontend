0301ef512c1cf988c03a126020cffd7afb93f9d6
KBG1 <bumdoly200@naver.com>
2025-06-21 00:14:52 +0900

    fix : 빌드 오류 수정

--- src/app/creator/courses/course-management/page.tsx
+++ src/app/creator/courses/course-management/page.tsx
@@ -1,7 +1,11 @@
 import CourseManagement from "../_component/manage/CourseManagement";
-
+import { Suspense } from "react";
 function CourseManagementPage() {
-	return <CourseManagement />;
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<CourseManagement />
+		</Suspense>
+	);
 }
 
 export default CourseManagementPage;
--- src/app/creator/courses/course-upload/page.tsx
+++ src/app/creator/courses/course-upload/page.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import CourseUpload from "../_component/upload/CourseUpload";
 
 function CourseUploadPage() {
-	return <CourseUpload />;
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<CourseUpload />
+		</Suspense>
+	);
 }
 
 export default CourseUploadPage;
