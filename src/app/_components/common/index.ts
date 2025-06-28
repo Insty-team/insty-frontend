8fbad953b3e50c9fbc2468d20f1e5dcc774af4de
dev-junehee <dev.junehee@gmail.com>
2025-06-28 14:49:41 +0900

    design: 러너 > 마이페이지 > AI 질문 이력 디자인 초안 퍼블리싱

--- src/app/_components/common/index.ts
+++ src/app/_components/common/index.ts
@@ -1,6 +1,8 @@
 import BaseButton from "./BaseButton";
 import BaseSearchBar from "./BaseSearchBar";
+import BaseSelect from "./BaseSelect";
 import BaseTab from "./BaseTab";
+import BaseTag from "./BaseTag";
 import CommunityVideo from "./CommunityVideo";
 import CreatorHeader from "./CreatorHeader";
 import LearnerHeader from "./LearnerHeader";
@@ -8,7 +10,9 @@ import LearnerHeader from "./LearnerHeader";
 export {
   BaseButton,
   BaseSearchBar,
+  BaseSelect,
   BaseTab,
+  BaseTag,
   CommunityVideo,
   CreatorHeader,
   LearnerHeader,
