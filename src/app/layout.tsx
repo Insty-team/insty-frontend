9c7974e8dce68ac8452141fd626d1838bdf92515
KBG1 <bumdoly200@naver.com>
2025-07-02 13:52:41 +0900

    fix : 비로그인 상태에도 로그인 이후 페이지 들어갈 수 있는 문제 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,11 @@
 import "./globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
+import { PUBLIC_PAGE_PATH } from "./constants";
 import Providers from "./Providers";
 
 function RootLayout({
@@ -12,12 +16,32 @@ function RootLayout({
 	children: React.ReactNode;
 }>) {
 	const queryClient = new QueryClient();
+	const pathname = usePathname();
+	const router = useRouter();
+	const [checked, setChecked] = useState(false);
+
+	useEffect(() => {
+		const token = localStorage.getItem("@insty-app.accessToken");
+		const isPublicPage = PUBLIC_PAGE_PATH.includes(pathname);
+		if (!token && !isPublicPage) {
+			Swal.fire({
+				title: "로그인이 필요한 페이지입니다.",
+				icon: "error",
+				confirmButtonText: "확인",
+			}).then(() => {
+				router.push("/login");
+			});
+		} else {
+			setChecked(true);
+		}
+	}, [pathname, router]);
+
 	return (
 		<html lang="ko">
 			<body className="font-sans flex-1">
 				<main>
 					<QueryClientProvider client={queryClient}>
-						<Providers>{children}</Providers>
+						<Providers>{checked ? children : null}</Providers>
 					</QueryClientProvider>
 				</main>
 			</body>
