539d705a75a51fb826cb73f6e7967715573c1436
KBG1 <bumdoly200@naver.com>
2025-07-28 18:35:17 +0900

    refactor : 강의 목록 부분 로딩 스패너 및 라우팅 방식 변경 (useEffect -> useLayoutEffect)

--- src/app/creator/courses/course-management/page.tsx
+++ src/app/creator/courses/course-management/page.tsx
@@ -1,9 +1,17 @@
 import { Suspense } from "react";
 
+import Loading from "@/app/_components/common/Loading";
+
 import CourseManagement from "../_component/manage/CourseManagement";
 function CourseManagementPage() {
 	return (
-		<Suspense fallback={""}>
+		<Suspense
+			fallback={
+				<div className="flex justify-center items-center h-full">
+					<Loading />
+				</div>
+			}
+		>
 			<CourseManagement />
 		</Suspense>
 	);
