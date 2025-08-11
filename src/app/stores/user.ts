ac0086aeb0335992f9ca9033f8e5ec66d40c3d59
fbgjung <fbgjung@gmail.com>
2025-08-12 01:36:15 +0900

    feat: #135 userType 상태 유지를 위해 user stores persist 미들웨어 추가

--- src/app/stores/user.ts
+++ src/app/stores/user.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 import { UserInfo } from "@/app/types/index.d";
 
@@ -10,41 +11,48 @@ interface UserStore {
 	resetUser: () => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-	user: {
-		nickname: "",
-		introduce: "",
-		userType: "",
-	},
-
-	// 유저 정보 저장
-	setUser: (user) => set({ user }),
-
-	// 유저 타입만 설정
-	setUserType: (userType: string) =>
-		set((state) => ({
-			user: {
-				...state.user,
-				userType,
-			},
-		})),
-
-	//유저 소개만 설정(크리에이터만 가능)
-	setUserDescription: (description: string) =>
-		set((state) => ({
-			user: {
-				...state.user,
-				description,
-			},
-		})),
-
-	// 유저 정보 삭제
-	resetUser: () =>
-		set({
+export const useUserStore = create<UserStore>()(
+	persist(
+		(set) => ({
 			user: {
 				nickname: "",
 				introduce: "",
 				userType: "",
 			},
+
+			// 유저 정보 저장
+			setUser: (user) => set({ user }),
+
+			// 유저 타입만 설정
+			setUserType: (userType: string) =>
+				set((state) => ({
+					user: {
+						...state.user,
+						userType,
+					},
+				})),
+
+			//유저 소개만 설정(크리에이터만 가능)
+			setUserDescription: (description: string) =>
+				set((state) => ({
+					user: {
+						...state.user,
+						description,
+					},
+				})),
+
+			// 유저 정보 삭제
+			resetUser: () =>
+				set({
+					user: {
+						nickname: "",
+						introduce: "",
+						userType: "",
+					},
+				}),
 		}),
-}));
+		{
+			name: "user_info",
+		},
+	),
+);
