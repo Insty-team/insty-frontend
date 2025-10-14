8b0fc6b7d1361f466c9fd507ec02a07cb9a255c7
injulme <injulme0309@naver.com>
2025-10-14 22:25:05 +0900

    initial setting

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+export default function TestPage() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
