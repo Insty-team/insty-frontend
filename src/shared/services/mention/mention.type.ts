c72112fa9246d838f0a326536344fc9368c1d601
fbgjung <fbgjung@gmail.com>
2026-01-01 19:16:38 +0900

    feat: mention api 수정

--- src/shared/services/mention/mention.type.ts
+++ src/shared/services/mention/mention.type.ts
@@ -1,7 +1,7 @@
 import { UserType } from '@/shared/types/auth.enum';
 
 export type MentionSearchResponse = {
-  id: string;
+  id: number;
   nickname: string;
   userType: UserType;
   profileImageUrl: string;
